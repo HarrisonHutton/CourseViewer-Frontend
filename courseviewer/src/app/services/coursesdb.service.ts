97768fe8d15f8868ff8f32b183d595d1e3e38dc4
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-13 00:14:43 -0500

    ✨ Create Coursesdb service

---
+++ courseviewer/src/app/services/coursesdb.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesdbService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllCourses() {
+    return this.http.get('http://localhost:5000/api/courses');
+  }
+
+  getDptCourses(dpt: string) {
+    return this.http.get(`http://localhost:5000/api/courses/${dpt}`);
+  }
+}
