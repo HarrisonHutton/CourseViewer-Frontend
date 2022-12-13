e8bc004ca930e949340a1c1ff2eb119834deb7b4
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-13 12:50:20 -0500

    🎉 Generate SearchOptions component

--- courseviewer/src/app/app.module.ts
+++ courseviewer/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesViewComponent } from './courses-view/courses-view.component';
-import { CoursesdbService } from './services/coursesdb.service';
+import { CoursesdbService } from '../utils/services/coursesdb.service';
+import { SearchOptionsComponent } from './search-options/search-options.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesViewComponent,
+    SearchOptionsComponent,
   ],
   imports: [
     BrowserModule,
