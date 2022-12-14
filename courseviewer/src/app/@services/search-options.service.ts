4bf9829513db0e0d488ad8e9d795f3d2559aa2e4
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-13 21:03:28 -0500

    ✨ Create SearchOptions service

--- courseviewer/src/app/@services/search-options.service.ts
+++ courseviewer/src/app/@services/search-options.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchOptionsService {
 
+  private departmentSource = new BehaviorSubject<string>("CSE");
+  currentDpt = this.departmentSource.asObservable();
+
   constructor() { }
+
+  changeDpt(dpt: string) {
+    this.departmentSource.next(dpt);
+  }
+
 }
